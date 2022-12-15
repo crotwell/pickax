9946223ab141ee02984b786a0cdf4e5ac16b60b5
Philip Crotwell <crotwell@seis.sc.edu>
2022-12-15 10:20:37 -0500

    reload usgs events to get picks

--- src/pickax/__init__.py
+++ src/pickax/__init__.py
@@ -1,7 +1,9 @@
+from .pick_util import reloadQuakeMLWithPicks, extractEventId
 from .pickax import PickAx
 from .blit_manager import BlitManager
 __all__ = [
     PickAx,
     BlitManager,
-
+    reloadQuakeMLWithPicks,
+    extractEventId,
 ]
