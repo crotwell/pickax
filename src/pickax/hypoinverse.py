a943c92713aeee84a40506d767387e43035fd1ea
Philip Crotwell <crotwell@seis.sc.edu>
2023-02-28 16:02:30 -0500

    generate phs file from picks

--- src/pickax/hypoinverse.py
+++ src/pickax/hypoinverse.py
@@ -7,7 +7,31 @@ from pathlib import Path
 import argparse
 from obspy import read_events, Catalog, read_inventory, Inventory
 
+def qml_to_phs_header(quake):
+    otime = quake.preferred_origin().time
+    ymdhms = otime.strftime("%Y%m%d%H%M")
+    osec = otime.second*100+ int(otime.microsecond/10000)
+    lat = "34N 600" # 34. N, 6 min
+    lon = " 80W1000" # -80 W 10 min
+    depth ="  000"
+    return f"{ymdhms}{osec:>5d}{lat}{lon}{depth}"
 
+def pick_to_phs(pick):
+    weight = "4"
+    wid = pick.waveform_id
+    sourceId = f"{wid.station_code:<5}{wid.network_code:<2}  {wid.channel_code:<3}"
+    p_ymdhm = pick.time.strftime("%Y%m%d%H%M")
+    psec = pick.time.second*100+ int(pick.time.microsecond/10000)
+    pwave = f"   {weight}{p_ymdhm}    0   0  0"
+    swave = f"    0   0  0"
+    if pick.phase_hint == "P":
+        pwave = f" P {weight}{p_ymdhm}{psec:>5d}   0"
+    elif pick.phase_hint == "S":
+        swave = f"{psec:>5d} S {weight}"
+    else:
+        raise Error("Unknown phase hint"+p.phase_hint)
+    otherstuff = "  18      0 0 40   0 -16  1215200  0     248  0  0   0  23J  "
+    return f"{sourceId} {pwave}{swave}{otherstuff}{wid.location_code}"
 
 def format_hypoinverse(inv):
     lines = []
@@ -39,6 +63,13 @@ def do_parseargs():
         help="QuakeML file to load",
     )
     parser.add_argument(
+        "-a",
+        "--authors",
+        nargs='*',
+        required=False,
+        help="Authors of picks to pull from QuakeML file",
+    )
+    parser.add_argument(
         "-s",
         "--staxml",
         required=False,
@@ -55,10 +86,16 @@ def do_parseargs():
 def main():
     args = do_parseargs()
     bad_file_chars_pat = re.compile(r'[\s:\(\)/]+')
+    outdir = Path(".")
+    if args.dir is not None:
+        outdir = Path(args.dir)
+        if not outdir.exists():
+            outdir.mkdir(parents=True, exist_ok=True)
     if args.staxml:
         inv = read_inventory(args.staxml)
         lines = format_hypoinverse(inv)
-        with open(f"{args.staxml}.sta", "w") as f:
+        outfile = Path(outdir / f"{args.staxml}.sta")
+        with open(outfile, "w") as f:
             for l in lines:
                 f.write(f"{l}\n")
     if args.quakeml:
@@ -70,29 +107,19 @@ def main():
         else:
             print(f"File {args.quakeml} does not seem to exist, cowardly quitting...")
             return
-        if args.dir is not None and not os.path.exists(args.dir):
-            Path(args.dir).mkdir(parents=True, exist_ok=True)
         catalog = read_events(args.quakeml)
-        for idx, qmlevent in enumerate(catalog):
-            if args.dir is not None:
-                dirPath = Path(args.dir)
-                filename = origin_mag_to_string(qmlevent).strip()
-                filename = re.sub(bad_file_chars_pat, '_', filename)
-                filePath = Path(dirPath / filename)
-                if not filePath.exists():
-                    # only load picks if event file doesn't already exist
-                    reloaded = reloadQuakeMLWithPicks(qmlevent, host=args.dc)
-                    single_cat = Catalog([reloaded])
-                    single_cat.write(filePath, format="QUAKEML")
-                    if args.verbose:
-                        print(f"Save {filePath}")
-            else:
-                reloaded = reloadQuakeMLWithPicks(qmlevent, host=args.dc)
-                catalog[idx] = reloaded
-        if args.dir is None:
-            if saved_file is not None:
-                os.rename(catalog_file, saved_file)
-            catalog.write(catalog_file, format="QUAKEML")
+        outfile = Path(outdir / f"{args.quakeml}.phs")
+        with open(outfile, "w") as phsfile:
+            for idx, quake in enumerate(catalog):
+                phsfile.write(f"{qml_to_phs_header(quake)}\n")
+                #phsfile.write("SB4  BG  DPZ IPU1201001030833  826   1101    0   0   0      0 0  0  -9   0  1215205  0 54.0248  0  0  53   0J  -- 0DPZ X\n")
+                #phsfile.write("SB4  BG  DPE    4201001030833    0   0  0  881ES 2  18      0 0 40   0 -16  1215200  0     248  0  0   0  23J  -- 0DPE\n")
+                for pick in quake.picks:
+                    if args.authors is None or len(args.authors) == 0 or pick.creation_info.author in args.authors:
+                        phsfile.write(f"{pick_to_phs(pick)}\n")
+
+        #if args.hypodd:
+        #    catalog.write(catalog_file, format="HYPODD")
     print("Done")
 
 
