769e46830f7d71365b3261be68ecee3aebd42097
Philip Crotwell <crotwell@seis.sc.edu>
2023-03-03 12:33:11 -0500

    output staml for node stn file

--- node_iterate.py
+++ node_iterate.py
@@ -135,6 +135,7 @@ class NodeSacZips(SeismogramIterator):
 def main():
     all = load_nodes("stn_node_all_update.txt")
     print(len(all))
+    all.write("stn_node_all_update.qml", format="STATIONXML")
 
 
 if __name__ == "__main__":
